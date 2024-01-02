e33e392ca93a4c64bf9b28782eb59060eb67f02a
Amin Mousavi <seyedamin963@gmail.com>
2024-01-02 14:16:10 +0330

    Enter new project information and show projects in sidebar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,55 @@
+import { useState } from "react";
+
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectSidebar";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    const projectId = math.random();
+    const newProject = {
+      ...projectData,
+      id: projectId,
+    };
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  console.log(projectState);
+
+  let content;
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      {/* <NewProject /> */}
-      <NoProjectSelected />
+      <ProjectSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectState.projects}
+      />
+      {content}
     </main>
   );
 }
--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,7 +1,7 @@
 import Button from "./Button"
 import noProjectImage from "../assets/no-projects.png";
 
-function NoProjectSelected() {
+function NoProjectSelected({onStartAddProject}) {
   return (
     <div className="mt-24 text-center w-2/3 ">
       <img
@@ -16,7 +16,7 @@ function NoProjectSelected() {
         Select a project or get start a new project
       </p>
       <p className="mt-8 ">
-        <Button>Create new project</Button>
+        <Button onClick={onStartAddProject}>Create new project</Button>
       </p>
     </div>
   );
